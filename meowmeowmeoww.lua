4904d6070155d3b2cbd4f07a9d82845cac5dc228
yuuto <yourgaydad2020@gmail.com>
2025-07-04 18:24:16 +1000

    Update meowmeowmeoww.lua

--- meowmeowmeoww.lua
+++ meowmeowmeoww.lua
@@ -1,1 +1,1 @@
-game.Players.LocalPlayer.Kick("Script ported to different github, please DM me if you haven't recieved it. yuutoes. on discord.")
+game.Players.LocalPlayer:Kick("Script ported to different github, please DM me if you haven't recieved it. yuutoes. on discord.")
